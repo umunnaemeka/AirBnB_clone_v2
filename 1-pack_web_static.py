6c49b22ac527dda66fb4320b002b245065f79f52
umunnaemeka <umunnaemeka@gmail.com>
2023-04-08 00:30:55 -0700

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
 from fabric.api import local
-import os
+from time import strftime
+from datetime import date
 
 
 def do_pack():
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
     try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
