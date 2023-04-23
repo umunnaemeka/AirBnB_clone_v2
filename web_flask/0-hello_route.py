284b69358664b03bd2e692af7f0525a64eeda9a6
umunnaemeka <umunnaemeka@gmail.com>
2023-04-23 04:49:30 -0700

    Web Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    name = request.args.get("name", "Hello HBNB")
+
     """ Function called with / route """
-    return 'Hello HBNB!'
+    return 'Hello HBNB, {escape(name)}! '
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
