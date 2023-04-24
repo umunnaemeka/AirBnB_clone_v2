87f03ae1d44efecf2553abb6edcac1611b85781a
umunnaemeka <umunnaemeka@gmail.com>
2023-04-24 16:25:30 -0700

    Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
-from flask import Flask, render_template
+"""Start web application with two routings
+"""
+
 from models import storage
 from models.state import State
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/states_list')
+def states_list():
+    """Render template with states
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
